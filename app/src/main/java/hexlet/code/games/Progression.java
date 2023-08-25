65a79ad39a43aff373ef5da77cf54e65a3f65f81
blonde <mr_blonde@rambler.ru>
2023-08-25 19:53:01 +0700

    fixed linter errors

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -12,23 +12,13 @@ public class Progression {
         Random random = new Random();
         for (var i = 0; i < attempts; i++) {
             //подготовим данные для задачи
-            int arrayLength = random.nextInt(minRandomValue, maxRandomValue);
-            String array = "";
             int startNumber = random.nextInt(maxRandomValue);
             int progressionNumber = random.nextInt(1, maxRandomValue);
+            int arrayLength = random.nextInt(minRandomValue, maxRandomValue);
             int hiddenIndex = random.nextInt(arrayLength);
-            String correctAnswer = "";
-            //заполним строку чисел
-            for (var n = 0; n < arrayLength; n++) {
-                if (n == hiddenIndex) {
-                    correctAnswer = "" + startNumber; //это будет правильный ответ
-                    startNumber = startNumber + progressionNumber;
-                    array = array + " ..";
-                } else {
-                    array = array + " " + startNumber;
-                    startNumber = startNumber + progressionNumber;
-                }
-            }
+            String array = getArray(arrayLength, startNumber, progressionNumber, hiddenIndex);
+            //вычислим правильный ответ
+            String correctAnswer = getCorrectAnswer(arrayLength, startNumber, progressionNumber, hiddenIndex);
             //выводим вопрос и считываем ответ
             String question = "Question:" + array;
             String answer = Engine.getAnswer(question);
@@ -38,5 +28,29 @@ public class Progression {
             }
         }
         System.out.println("Congratulations, " + name + "!");
+    }
+    public static String getArray(int arrayLength, int startNumber, int progressionNumber, int hiddenIndex) {
+        String array = "";
+        for (var n = 0; n < arrayLength; n++) {
+            if (n == hiddenIndex) {
+                startNumber = startNumber + progressionNumber;
+                array = array + " ..";
+            } else {
+                array = array + " " + startNumber;
+                startNumber = startNumber + progressionNumber;
+            }
+        }
+        return array;
+    }
+    public static String getCorrectAnswer(int arrayLength, int startNumber, int progressionNumber, int hiddenIndex) {
+        String correctAnswer = "";
+        for (var n = 0; n < arrayLength; n++) {
+            if (n == hiddenIndex) {
+                correctAnswer = "" + startNumber; //это будет правильный ответ
+                startNumber = startNumber + progressionNumber;
+            }
+            startNumber = startNumber + progressionNumber;
+        }
+        return correctAnswer;
     }
 }
